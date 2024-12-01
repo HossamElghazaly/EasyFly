2827433e7691defb5ad0ae82a51447ddd8467bc3
Fady <fadybsuony@gmail.com>
2024-12-01 22:43:50 +0200

    JavaScript added

---
+++ frontend/Interface.js
@@ -0,0 +1,11 @@
+// Go to Registration Page
+document.getElementById("createAccountLink")?.addEventListener("click", function (e) {
+    e.preventDefault();
+    window.location.href = "easy fly 2.html"; // Redirects to the registration page
+});
+
+// Go to Login Page
+document.getElementById("signInLink")?.addEventListener("click", function (e) {
+    e.preventDefault();
+    window.location.href = "easy fly 1.html"; // Redirects to the login page
+});
